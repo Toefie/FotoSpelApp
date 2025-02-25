d72fcab2aed4f686741752c32ed45a89c87a2496
lorenzo Pineda Olzheim <2304141pinedaolzheim@zuyd.nl>
2025-02-25 09:17:15 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FotoSpelApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
